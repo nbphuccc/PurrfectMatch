ddea3fa679c8f2415039a1e845241bb1728b16dd
Xuan-NhuT <xuannhu1406@gmail.com>
2025-10-19 18:12:58 -0700

    add Stack.screen for both signup and forgotPassword so it show 'back' next to the arrow and its title

--- purrfectMatch-Homepage/app/_layout.tsx
+++ purrfectMatch-Homepage/app/_layout.tsx
@@ -16,6 +16,8 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="forgotPassword" options={{ title: 'Reset Password', headerBackTitle: 'Back'}} />
+        <Stack.Screen name="signup" options={{ title: 'Sign up', headerBackTitle: 'Back' }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
