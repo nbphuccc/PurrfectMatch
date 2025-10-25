3c0bbc3a752c2894da202820a7221053a00a7f8d
AbigailLew4444 <abigailclew@gmail.com>
2025-10-24 18:45:02 -0700

    updated db schema with living doc schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -3,22 +3,35 @@ CREATE TABLE IF NOT EXISTS users(
   email TEXT UNIQUE NOT NULL,
   username TEXT UNIQUE NOT NULL,
   pass_hash TEXT NOT NULL,
-  name TEXT, bio TEXT, city TEXT,
+  name TEXT,
+  bio TEXT,
   email_public INTEGER DEFAULT 0,
-  avatar_url TEXT, created_at TEXT
+  avatar_url TEXT,
+  created_at TEXT
 );
-
-CREATE TABLE IF NOT EXISTS posts(
+CREATE TABLE IF NOT EXISTS playdate_posts(
   id INTEGER PRIMARY KEY,
   author_id INTEGER REFERENCES users(id),
-  type TEXT CHECK(type IN('playdate','community')),
-  title TEXT, description TEXT NOT NULL,
-  pet_type TEXT, city TEXT, zip TEXT,
-  when_at TEXT, place TEXT,
-  contact_email TEXT,
-  image_url TEXT, created_at TEXT, expires_at TEXT
+  title TEXT NOT NULL,
+  description TEXT NOT NULL,
+  dog_breed TEXT NOT NULL,
+  address TEXT NOT NULL,
+  city TEXT NOT NULL,
+  state TEXT NOT NULL,
+  zip TEXT NOT NULL,
+  when_at TEXT NOT NULL,
+  place TEXT NOT NULL,
+  image_url TEXT,
+  created_at TEXT NOT NULL
 );
-
+CREATE TABLE IF NOT EXISTS community_posts(
+  id INTEGER PRIMARY KEY,
+  author_id INTEGER REFERENCES users(id),
+  title TEXT,
+  description TEXT NOT NULL,
+  image_url TEXT,
+  created_at TEXT NOT NULL
+);
 CREATE TABLE IF NOT EXISTS comments(
   id INTEGER PRIMARY KEY,
   post_id INTEGER REFERENCES posts(id),
