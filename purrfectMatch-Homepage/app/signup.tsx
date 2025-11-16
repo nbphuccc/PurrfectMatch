ae3d5af7f01c55b319c4ad648a0bd5ea7fb8a4ca
Bao Phuc <nbphucc@gmail.com>
2025-11-15 22:46:21 -0800

    capitalize signup

--- purrfectMatch-Homepage/app/signup.tsx
+++ purrfectMatch-Homepage/app/signup.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import { StyleSheet, Text, TextInput, TouchableOpacity, View, Alert } from 'react-native'
 import { signup } from "../api/auth";
 
-export default function signUp() {
+export default function SignUp() {
   const router = useRouter()
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
