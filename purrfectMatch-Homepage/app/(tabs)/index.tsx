6a867409ada2705609e18d068cc4cd3366b9fa2e
Larrianna <larriannasalgadowarner@gmail.com>
2025-12-07 16:50:24 -0800

    Removed loading screens between tabs (#100)

--- purrfectMatch-Homepage/app/(tabs)/index.tsx
+++ purrfectMatch-Homepage/app/(tabs)/index.tsx
@@ -297,20 +297,6 @@ export default function CommunityScreen() {
     setShowForm(true);
   };
 
-  if (loading) {
-    return (
-      <View style={styles.fullScreenLoading}>
-        <Image
-          source={{
-            uri: 'https://media.istockphoto.com/id/1444657782/vector/dog-and-cat-profile-logo-design.jpg?s=612x612&w=0&k=20&c=86ln0k0egBt3EIaf2jnubn96BtMu6sXJEp4AvaP0FJ0=',
-          }}
-          style={styles.loadingImage}
-        />
-        <ActivityIndicator size="large" color="#3498db" style={styles.loadingSpinner} />
-      </View>
-    );
-  }
-
   return (
     <KeyboardAvoidingView
     style={{ flex: 1 }}
@@ -526,25 +512,5 @@ const styles = StyleSheet.create({
     shadowColor: '#000',
     shadowOpacity: 0.25,
     shadowRadius: 4
-  },
-  fullScreenLoading: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: '#fff', // or semi-transparent like 'rgba(255,255,255,0.9)'
-    justifyContent: 'center',
-    alignItems: 'center',
-    zIndex: 9999, // ensure it sits on top
-  },
-  loadingImage: {
-    width: 120,
-    height: 120,
-    borderRadius: 60,
-    marginBottom: 20,
-  },
-  loadingSpinner: {
-    marginTop: 10,
   },
 });
